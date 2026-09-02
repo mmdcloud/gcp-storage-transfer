54bc99095121b9aa184d4549eb673325e9a2538c
mmdcloud <madmaxcloudpractice@gmail.com>
2026-09-02 09:37:54 +0530

    fix: source and bucket folder paths in replication usage module

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,8 +3,6 @@ locals {
   start_year        = formatdate("YYYY", local.current_timestamp)
   start_month       = formatdate("MM", local.current_timestamp)
   start_day         = formatdate("DD", local.current_timestamp)
-  hour              = formatdate("HH", local.current_timestamp)
-  minute            = formatdate("mm", local.current_timestamp)
 
   # Add 24h as an actual duration, then derive the end date from the result
   end_timestamp = timeadd(local.current_timestamp, "24h")
@@ -31,143 +29,6 @@ resource "random_id" "id" {
 
 # -------------------------------------------------------------------------------
 # Scheduled transfer usage 
-# -------------------------------------------------------------------------------
-module "source_bucket" {
-  source        = "./modules/gcs"
-  name          = "source-bucket-${var.project_id}"
-  storage_class = "STANDARD"
-  location      = "asia-south1"
-  force_destroy = true
-  cors = [
-    {
-      origin          = ["*"]
-      max_age_seconds = 3600
-      method          = ["GET"]
-      response_header = ["*"]
-    }
-  ]
-  contents = [
-    {
-      name        = "image-1.jpg",
-      content     = ""
-      source_path = "../src/image-1.jpg"
-    },
-    {
-      name        = "image-2.jpg",
-      content     = ""
-      source_path = "../src/image-2.jpg"
-    },
-    {
-      name        = "image-3.jpg",
-      content     = ""
-      source_path = "../src/image-3.jpg"
-    }
-  ]
-}
-
-module "destination_bucket" {
-  source        = "./modules/gcs"
-  name          = "destination-bucket-${var.project_id}"
-  storage_class = "STANDARD"
-  location      = "asia-south2"
-  force_destroy = true
-  cors = [
-    {
-      origin          = ["*"]
-      max_age_seconds = 3600
-      method          = ["PUT"]
-      response_header = ["*"]
-    }
-  ]
-  contents = []
-}
-
-resource "google_storage_bucket_iam_member" "source_bucket_object_viewer" {
-  bucket     = module.source_bucket.bucket_name
-  role       = "roles/storage.objectViewer"
-  member     = "serviceAccount:${data.google_storage_transfer_project_service_account.default.email}"
-  depends_on = [module.source_bucket]
-}
-
-resource "google_storage_bucket_iam_member" "source_bucket_legacy_reader" {
-  bucket     = module.source_bucket.bucket_name
-  role       = "roles/storage.legacyBucketReader"
-  member     = "serviceAccount:${data.google_storage_transfer_project_service_account.default.email}"
-  depends_on = [module.source_bucket]
-}
-
-resource "google_storage_bucket_iam_member" "destination_bucket_legacy_writer" {
-  bucket     = module.destination_bucket.bucket_name
-  role       = "roles/storage.legacyBucketWriter"
-  member     = "serviceAccount:${data.google_storage_transfer_project_service_account.default.email}"
-  depends_on = [module.destination_bucket]
-}
-
-module "topic" {
-  source     = "./modules/pubsub"
-  topic_name = var.pubsub_topic_name
-}
-
-resource "google_pubsub_topic_iam_member" "notification_config" {
-  topic  = module.topic.id
-  role   = "roles/pubsub.publisher"
-  member = "serviceAccount:${data.google_storage_transfer_project_service_account.default.email}"
-}
-
-module "storage_transfer_service" {
-  source      = "./modules/storage-transfer"
-  name        = "transferJobs/storagetransfer"
-  description = "Storage Transfer Service"
-
-  transfer_spec = {
-    gcs_data_sink = {
-      bucket_name = module.destination_bucket.bucket_name
-    }
-    gcs_data_source = {
-      bucket_name = module.source_bucket.bucket_name
-    }
-    transfer_options = {
-      delete_objects_unique_in_sink = false
-    }
-  }
-
-  schedule = [
-    {
-      start_year               = local.start_year
-      start_month              = local.start_month
-      start_day                = local.start_day
-      end_year                 = local.end_year
-      end_month                = local.end_month
-      end_day                  = local.end_day
-      hours                    = local.schedule_hour
-      minutes                  = local.schedule_minute
-      seconds                  = 0
-      nanos                    = 0
-      schedule_repeat_interval = "3600s"
-    }
-  ]
-
-  notification_config = [
-    {
-      pubsub_topic = module.topic.id
-      event_types = [
-        "TRANSFER_OPERATION_SUCCESS",
-        "TRANSFER_OPERATION_FAILED"
-      ]
-      payload_format = "JSON"
-    }
-  ]
-
-  depends_on = [
-    google_storage_bucket_iam_member.source_bucket_object_viewer,
-    google_storage_bucket_iam_member.source_bucket_legacy_reader,
-    google_storage_bucket_iam_member.destination_bucket_legacy_writer,
-    google_pubsub_topic_iam_member.notification_config
-  ]
-}
-
-# -------------------------------------------------------------------------------
-# Replication module usage 
 # -------------------------------------------------------------------------------
 # module "source_bucket" {
 #   source        = "./modules/gcs"
@@ -226,18 +87,11 @@ module "storage_transfer_service" {
 #   depends_on = [module.source_bucket]
 # }
 
-# resource "google_storage_bucket_iam_member" "source_bucket_legacy_owner" {
+# resource "google_storage_bucket_iam_member" "source_bucket_legacy_reader" {
 #   bucket     = module.source_bucket.bucket_name
-#   role       = "roles/storage.legacyBucketOwner"
+#   role       = "roles/storage.legacyBucketReader"
 #   member     = "serviceAccount:${data.google_storage_transfer_project_service_account.default.email}"
 #   depends_on = [module.source_bucket]
-# }
-
-# resource "google_storage_bucket_iam_member" "destination_bucket_object_admin" {
-#   bucket     = module.destination_bucket.bucket_name
-#   role       = "roles/storage.objectAdmin"
-#   member     = "serviceAccount:${data.google_storage_transfer_project_service_account.default.email}"
-#   depends_on = [module.destination_bucket]
 # }
 
 # resource "google_storage_bucket_iam_member" "destination_bucket_legacy_writer" {
@@ -245,22 +99,6 @@ module "storage_transfer_service" {
 #   role       = "roles/storage.legacyBucketWriter"
 #   member     = "serviceAccount:${data.google_storage_transfer_project_service_account.default.email}"
 #   depends_on = [module.destination_bucket]
-# }
-
-# resource "google_project_iam_member" "sts_pubsub_editor" {
-#   project = var.project_id
-#   role    = "roles/pubsub.editor"
-#   member  = "serviceAccount:${data.google_storage_transfer_project_service_account.default.email}"
-# }
-
-# data "google_storage_project_service_account" "gcs_sa" {
-#   project = var.project_id
-# }
-
-# resource "google_project_iam_member" "gcs_pubsub_publisher" {
-#   project = var.project_id
-#   role    = "roles/pubsub.publisher"
-#   member  = "serviceAccount:${data.google_storage_project_service_account.gcs_sa.email_address}"
 # }
 
 # module "topic" {
@@ -274,24 +112,38 @@ module "storage_transfer_service" {
 #   member = "serviceAccount:${data.google_storage_transfer_project_service_account.default.email}"
 # }
 
-# module "gcs_replication" {
+# module "storage_transfer_service" {
 #   source      = "./modules/storage-transfer"
-#   name        = null
-#   description = "Continuous replication from raw to processed bucket"
-#   status      = "ENABLED"
+#   description = "Storage Transfer Service"
 
-#   replication_spec = [{
-#     source_bucket_name = module.source_bucket.bucket_name
-#     sink_bucket_name   = module.destination_bucket.bucket_name
-#     source_path        = "/"
-#     sink_path          = "/"
+#   transfer_spec = {
+#     gcs_data_sink = {
+#       bucket_name = module.destination_bucket.bucket_name
+#     }
+#     gcs_data_source = {
+#       bucket_name = module.source_bucket.bucket_name
+#     }
 #     transfer_options = {
 #       delete_objects_unique_in_sink = false
 #     }
-#   }]
+#   }
 
-#   schedule     = [] # must stay empty with replication_spec
-#   event_stream = [] # must stay empty with replication_spec
+#   schedule = [
+#     {
+#       start_year               = local.start_year
+#       start_month              = local.start_month
+#       start_day                = local.start_day
+#       end_year                 = local.end_year
+#       end_month                = local.end_month
+#       end_day                  = local.end_day
+#       hours                    = local.schedule_hour
+#       minutes                  = local.schedule_minute
+#       seconds                  = 0
+#       nanos                    = 0
+#       schedule_repeat_interval = "3600s"
+#     }
+#   ]
+
 #   notification_config = [
 #     {
 #       pubsub_topic = module.topic.id
@@ -304,14 +156,159 @@ module "storage_transfer_service" {
 #   ]
 
 #   depends_on = [
-#     google_storage_bucket_iam_member.source_bucket_legacy_owner,
 #     google_storage_bucket_iam_member.source_bucket_object_viewer,
+#     google_storage_bucket_iam_member.source_bucket_legacy_reader,
 #     google_storage_bucket_iam_member.destination_bucket_legacy_writer,
-#     google_project_iam_member.sts_pubsub_editor,
-#     google_project_iam_member.gcs_pubsub_publisher,
 #     google_pubsub_topic_iam_member.notification_config
 #   ]
 # }
+
+# -------------------------------------------------------------------------------
+# Replication module usage 
+# -------------------------------------------------------------------------------
+module "source_bucket" {
+  source        = "./modules/gcs"
+  name          = "source-bucket-${var.project_id}"
+  storage_class = "STANDARD"
+  location      = "asia-south1"
+  force_destroy = true
+  cors = [
+    {
+      origin          = ["*"]
+      max_age_seconds = 3600
+      method          = ["GET"]
+      response_header = ["*"]
+    }
+  ]
+  contents = [
+    {
+      name        = "image-1.jpg",
+      content     = ""
+      source_path = "../src/image-1.jpg"
+    },
+    {
+      name        = "image-2.jpg",
+      content     = ""
+      source_path = "../src/image-2.jpg"
+    },
+    {
+      name        = "image-3.jpg",
+      content     = ""
+      source_path = "../src/image-3.jpg"
+    }
+  ]
+}
+
+module "destination_bucket" {
+  source        = "./modules/gcs"
+  name          = "destination-bucket-${var.project_id}"
+  storage_class = "STANDARD"
+  location      = "asia-south2"
+  force_destroy = true
+  cors = [
+    {
+      origin          = ["*"]
+      max_age_seconds = 3600
+      method          = ["PUT"]
+      response_header = ["*"]
+    }
+  ]
+  contents = []
+}
+
+resource "google_storage_bucket_iam_member" "source_bucket_object_viewer" {
+  bucket     = module.source_bucket.bucket_name
+  role       = "roles/storage.objectViewer"
+  member     = "serviceAccount:${data.google_storage_transfer_project_service_account.default.email}"
+  depends_on = [module.source_bucket]
+}
+
+resource "google_storage_bucket_iam_member" "source_bucket_legacy_owner" {
+  bucket     = module.source_bucket.bucket_name
+  role       = "roles/storage.legacyBucketOwner"
+  member     = "serviceAccount:${data.google_storage_transfer_project_service_account.default.email}"
+  depends_on = [module.source_bucket]
+}
+
+resource "google_storage_bucket_iam_member" "destination_bucket_object_admin" {
+  bucket     = module.destination_bucket.bucket_name
+  role       = "roles/storage.objectAdmin"
+  member     = "serviceAccount:${data.google_storage_transfer_project_service_account.default.email}"
+  depends_on = [module.destination_bucket]
+}
+
+resource "google_storage_bucket_iam_member" "destination_bucket_legacy_writer" {
+  bucket     = module.destination_bucket.bucket_name
+  role       = "roles/storage.legacyBucketWriter"
+  member     = "serviceAccount:${data.google_storage_transfer_project_service_account.default.email}"
+  depends_on = [module.destination_bucket]
+}
+
+resource "google_project_iam_member" "sts_pubsub_editor" {
+  project = var.project_id
+  role    = "roles/pubsub.editor"
+  member  = "serviceAccount:${data.google_storage_transfer_project_service_account.default.email}"
+}
+
+data "google_storage_project_service_account" "gcs_sa" {
+  project = var.project_id
+}
+
+resource "google_project_iam_member" "gcs_pubsub_publisher" {
+  project = var.project_id
+  role    = "roles/pubsub.publisher"
+  member  = "serviceAccount:${data.google_storage_project_service_account.gcs_sa.email_address}"
+}
+
+module "topic" {
+  source     = "./modules/pubsub"
+  topic_name = var.pubsub_topic_name
+}
+
+resource "google_pubsub_topic_iam_member" "notification_config" {
+  topic  = module.topic.id
+  role   = "roles/pubsub.publisher"
+  member = "serviceAccount:${data.google_storage_transfer_project_service_account.default.email}"
+}
+
+module "gcs_replication" {
+  source      = "./modules/storage-transfer"
+  name        = null
+  description = "Continuous replication from raw to processed bucket"
+  status      = "ENABLED"
+
+  replication_spec = [{
+    source_bucket_name = module.source_bucket.bucket_name
+    sink_bucket_name   = module.destination_bucket.bucket_name
+    source_path        = ""
+    sink_path          = ""
+    transfer_options = {
+      delete_objects_unique_in_sink = false
+    }
+  }]
+
+  schedule     = [] # must stay empty with replication_spec
+  event_stream = [] # must stay empty with replication_spec
+  notification_config = [
+    {
+      pubsub_topic = module.topic.id
+      event_types = [
+        "TRANSFER_OPERATION_SUCCESS",
+        "TRANSFER_OPERATION_FAILED"
+      ]
+      payload_format = "JSON"
+    }
+  ]
+
+  depends_on = [
+    google_storage_bucket_iam_member.source_bucket_legacy_owner,
+    google_storage_bucket_iam_member.source_bucket_object_viewer,
+    google_storage_bucket_iam_member.destination_bucket_legacy_writer,
+    google_project_iam_member.sts_pubsub_editor,
+    google_project_iam_member.gcs_pubsub_publisher,
+    google_pubsub_topic_iam_member.notification_config
+  ]
+}
 
 # -------------------------------------------------------------------------------
 # Event stream usage 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,7 @@
 variable "project_id" {
   type = string
 }
-variable "source_bucket_location" {
-  type = string
-}
-variable "destination_bucket_location" {
-  type = string
-}
+
 variable "pubsub_topic_name" {
   type = string
 }
